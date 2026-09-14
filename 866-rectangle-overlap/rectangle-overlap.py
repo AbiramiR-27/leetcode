1fabf9461258c3797d031444385f4878638aae11
Abirami R <abiramesh2023@gmail.com>
2026-09-14 12:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.89%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        overlap_x = rec1[0] < rec2[2] and rec2[0] < rec1[2]
+        overlap_y = rec1[1] < rec2[3] and rec2[1] < rec1[3]
+        
+        return overlap_x and overlap_y
