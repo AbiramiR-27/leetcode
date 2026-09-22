e6f94f53900cc3e4223ee6dbf24adb1a2e289dd7
Abirami R <abiramesh2023@gmail.com>
2026-09-22 10:20:56 +0530

    Time: 268 ms (21.63%) | Memory: 19.9 MB (67.37%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,21 +1,16 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        left = 0
-        right = 0
-        sets = set()
-        maxlen = 0
-
-        while right < len(s):
-            if s[right] not in sets :
-                sets.add(s[right])
-                currentlen = right -left + 1
-                maxlen = max(maxlen,currentlen)
-                right += 1
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l=0
+        r=0
+        sets=set()
+        max_len=0
+        while r < len(s):
+            if s[r] not in sets:
+                sets.add(s[r])
+                c=r-l+1
+                max_len=max(max_len,c)
+                r+=1
             else:
-                sets.remove(s[left])
-                left += 1
-        return maxlen
-
-
-        
-        
+                sets.remove(s[l])
+                l+=1
+        return max_len
